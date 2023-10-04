074a047340e91cf1e8880908c6968034598c0b27
70yamanaka <nao.ktm.12.25@icloud.com>
2023-10-04 11:15:03 +0900

    20231004_migrateの実行

--- politics-app/database/migrations/2023_10_03_083316_create_todos_table.php
+++ politics-app/database/migrations/2023_10_03_083316_create_todos_table.php
@@ -14,7 +14,8 @@ class CreateTodosTable extends Migration
     public function up()
     {
         Schema::create('todos', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('title');
             $table->timestamps();
         });
     }
