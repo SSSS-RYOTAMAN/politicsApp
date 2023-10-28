9d09dd41339845956777fd7ee490e59ef29e2e8a
70yamanaka <nao.ktm.12.25@icloud.com>
2023-10-28 09:34:39 +0900

    20231028_一覧表示仮作成

--- politics-app/resources/views/users/index.blade.php
+++ politics-app/resources/views/users/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app');
 
 @section('content')
+  @if ($users->isNotEmpty())
+    @foreach ($users as $item)
+      <p>{{ $item->name }}</p>
+      <p>{{ $item->sex }}</p>
+      <p>{{ $item->year }}</p>
+      <p>{{ $item->month }}</p>
+      <p>{{ $item->day }}</p>
+      <p>{{ $item->support }}</p>
+    @endforeach
+  @endif
+
   TOPページ
   <a href="{{ url('users/create') }}">新規作成</a>
 @endsection
